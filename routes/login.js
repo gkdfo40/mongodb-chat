98c530c3876caf0cc8683017bcb7d2250ae800c6
SeungRae Kim <gkdfo40@gmail.com>
2021-06-02 17:21:42 +0900

    fetch login

--- routes/login.js
+++ routes/login.js
@@ -13,21 +13,21 @@ router.post('/', function (req, res) {
 
     data.findOne({ userID: userid }, function (err, doc) {
         if (err) {
-            res.send({ resultcode: 101, msg: "ID가 일치하지 않습니다." });
+            res.json({ resultcode: 101, msg: "ID가 일치하지 않습니다." });
         }
         else if (doc) {
             hasher({ password: password, salt: doc.salt }, (err, pass, salt, hash) => {
                 if (hash === doc.password) {
                     console.log(doc.userID + " is logined");
-                    res.send({ resultcode: 100, userID: doc.userID, username: doc.username });
+                    res.json({ resultcode: 100, userID: doc.userID, username: doc.username });
                 }
                 else {
-                    res.send({ resultcode: 202, msg: "login failed" });
+                    res.json({ resultcode: 202, msg: "login failed" });
                 }
             });
         }
         else {
-            res.send({ resultcode: 204, msg: "ID가 일치하지 않습니다." });
+            res.json({ resultcode: 204, msg: "ID가 일치하지 않습니다." });
         }
     });
 });
